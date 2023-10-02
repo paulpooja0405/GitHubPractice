f57de346f14d232f04f7cf69e44da8e2dea337fd
paulpooja0405 <paulpooja0405@gmail.com>
2023-10-02 14:37:16 +0530

    This is Practice

---
+++ GitHubPractice/src/main/java/com/githubpractice/pagelayer/HomePage.java
@@ -0,0 +1,10 @@
+package com.githubpractice.pagelayer;
+
+public class HomePage {
+	
+	public static void main (String [] args)
+	{
+		System.out.println("Welcome all of you");
+	}
+
+}
---
+++ GitHubPractice/src/test/java/com/githubpractice/testlayer/HomeTest.java
@@ -0,0 +1,5 @@
+package com.githubpractice.testlayer;
+
+public class HomeTest {
+
+}
